05f4815d5d5709b89c6280ae165cb009f4a288b8
bibolasgit <152637349+bibolasgit@users.noreply.github.com>
2023-12-04 23:17:10 +0100

    Add files via upload

---
+++ print_food.java
@@ -0,0 +1,21 @@
+
+public class print_food {
+
+    public static void main(String[] args) {
+
+        System.out.println("\"I love pizza");
+        System.out.println("It's really good");
+        System.out.println();
+
+        // This is a comment
+
+        /*
+         * This 
+         * Is
+         * a
+         * comment
+         */
+
+    }
+
+}
